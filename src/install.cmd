d9cc2c83a1fd2c054a94cd909b0133dcad946f5d
Greg Hogan <gregjhogan@gmail.com>
2017-01-10 19:38:25 -0600

    updated selenium installer URL

--- src/install.cmd
+++ src/install.cmd
@@ -1,4 +1,4 @@
 IF NOT EXIST "%WEBROOT_PATH%selenium-server-standalone-2.52.0.jar" (
 	cd /D %WEBROOT_PATH%
-	curl -O http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar
+	curl -O http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar
 )
