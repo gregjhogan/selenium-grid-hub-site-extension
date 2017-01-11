02db627e9769aad0994c8335b10207b1d67bf6ed
Greg Hogan <gregjhogan@gmail.com>
2017-01-10 19:39:03 -0600

    updated selenium jar check

--- src/install.cmd
+++ src/install.cmd
@@ -1,4 +1,4 @@
-IF NOT EXIST "%WEBROOT_PATH%selenium-server-standalone-2.52.0.jar" (
+IF NOT EXIST "%WEBROOT_PATH%selenium-server-standalone-3.0.1.jar" (
 	cd /D %WEBROOT_PATH%
 	curl -O http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar
 )
